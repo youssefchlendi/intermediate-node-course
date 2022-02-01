55ca8b054bef1b2dc9e07612e15e73985f040cdb
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-02-02 00:58:44 +0100

    Add get user by id method

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const bodyParser= require('body-parser');
 const port=8000;
 const app= express();
 
+const User=require('./models/User');
+mongoose.connect('mongodb://localhost/userData');
+
 app.use(bodyParser.json());
 
 app.listen(port, ()=>{
@@ -12,13 +15,44 @@ app.listen(port, ()=>{
 
 // CREATE
 app.post('/users',(req,res)=>{
-  // User.create()
+  User.create(
+    {
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:req.body.newData.password
+    },
+    (err,data)=>{
+    if (err){
+      res.json({success: false,message: err})
+    } else if (!data){
+      res.json({success: false,message: "Not Found"})
+    } else {
+      res.json({success: true,data: data})
+    }
+  })
 })
 
 app.route('/users/:id')
 // READ
 .get((req,res)=>{
-  // User.findById()
+  User.findById(req.params.id,(err,data)=>{
+    if (err){
+      res.json({
+        success: false,
+        message: err
+      })
+    } else if (!data){
+      res.json({
+        success: false,
+        message: "Not Found"
+      })
+    } else {
+      res.json({
+        success: true,
+        data: data
+      })
+    }
+  })
 })
 // UPDATE
 .put((req,res)=>{
