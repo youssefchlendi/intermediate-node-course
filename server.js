36b50604ddad80159c3a6e303a2f6ff7183bda68
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-02-02 01:01:07 +0100

    add update user

--- server.js
+++ server.js
@@ -56,7 +56,35 @@ app.route('/users/:id')
 })
 // UPDATE
 .put((req,res)=>{
-  // User.findByIdAndUpdate()
+  User.findByIdAndUpdate(
+    req.params.id,
+    {
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:req.body.newData.password
+    },
+    {
+      new:true
+    },
+    (err,data)=>{
+      if (err){
+        res.json({
+          success: false,
+          message: err
+        })
+      } else if (!data){
+        res.json({
+          success: false,
+          message: "Not Found"
+        })
+      } else {
+        res.json({
+          success: true,
+          data: data
+        })
+      }
+    }
+  )
 })
 // DELETE
 .delete((req,res)=>{
