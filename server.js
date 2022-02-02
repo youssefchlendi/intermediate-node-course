d137aa2c10ddca5908d478765c36a342129e6dde
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-02-02 01:30:38 +0100

    add bcrypt salting and get all users uri

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
+const bcrypt = require('bcrypt');
 const port=8000;
 const app= express();
-
 const User=require('./models/User');
 mongoose.connect('mongodb://localhost/userData');
 
@@ -12,12 +12,21 @@ app.use(bodyParser.json());
 app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
 })
-
+app.get('/users',(req,res)=>{
+  User.find(
+    (err,data)=>{
+      sendResponse(res,err,data);
+  })
+})
 // CREATE
-app.post('/users',(req,res)=>{
+app.post('/users',async (req,res)=>{
+  const salt = await bcrypt.genSalt(10);
+  // now we set user password to hashed password
   User.create(
     {
-      ...req.body.newData
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:await bcrypt.hash(req.body.newData.password, salt)
     },
     (err,data)=>{
       sendResponse(res,err,data);
@@ -32,11 +41,15 @@ app.route('/users/:id')
   })
 })
 // UPDATE
-.put((req,res)=>{
+.put(async(req,res)=>{
+  const salt = await bcrypt.genSalt(10);
   User.findByIdAndUpdate(
     req.params.id,
     {
-      ...req.body.newData
+      ...req.body.newData,
+
+      password:await bcrypt.hash(req.body.newData.password, salt)
+
     },
     {
       new:true
@@ -73,4 +86,13 @@ function sendResponse(res,err,data){
       data: data
     })
   }
+}
+function crypt(password,rounds){
+  bcrypt.hash(password, rounds, (err, hash) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    return hash
+  })
 }
